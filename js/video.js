ba61fd4a5421783349d7036f9644955d5f7216e4
Linus Xu <linus_xu@live.com>
2024-03-26 01:17:09 -0400

    Main submission

--- js/video.js
+++ js/video.js
@@ -1,11 +1,57 @@
-var video;
+const video = document.querySelector('#player1');
 
-window.addEventListener("load", function() {
-	console.log("Good job opening the window")
+window.addEventListener("load", () => {
+	video.autoplay = false;
+	video.loop = false;
+	document.querySelector('#volume').textContent = '100%';
+})
 
-});
+document.querySelector('#play').addEventListener('click', () => {
+	video.play();
+})
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector('#pause').addEventListener('click', () => {
+	video.pause();
+})
 
+document.querySelector('#slower').addEventListener('click', () => {
+	video.playbackRate *= 0.9;
+	console.log(video.playbackRate);
+})
+
+document.querySelector('#faster').addEventListener('click', () => {
+	video.playbackRate /= 0.9;
+	console.log(video.playbackRate);
+})
+
+document.querySelector('#skip').addEventListener('click', () => {
+	console.log(currentTime);
+	if (video.duration - video.currentTime <= 10){
+		video.currentTime = 0;
+	}else{
+		video.currentTime += 10;
+	}
+})
+
+document.querySelector('#mute').addEventListener('click', () => {
+	if (video.muted) {
+		document.querySelector('#mute').textContent = 'Mute';
+		video.muted = false;
+	}else{
+		document.querySelector('#mute').textContent = 'Unmute';
+		video.muted = true;
+	}
+})
+
+document.querySelector('#slider').addEventListener('change', (e) => {
+	video.volume = e.target.value / 100;
+	document.querySelector('#volume').textContent = `${e.target.value}%`;
+})
+
+document.querySelector('#vintage').addEventListener('click', () => {
+	video.classList.add('oldSchool');
+})
+
+document.querySelector('#orig').addEventListener('click', () => {
+	video.classList.remove('oldSchool');
+})
